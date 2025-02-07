363d45893a5a934706792dcc672124404511f964
thomas1996 <thomas.kanthack@gmail.com>
2025-02-07 15:32:06 +0100

    add bg to rs

--- src/app/components/RSComp.tsx
+++ src/app/components/RSComp.tsx
@@ -1,12 +1,15 @@
+"use client";
 import { urlFor } from "@/sanity/lib/image";
 import { Setting } from "@/sanity/types";
 import Image from "next/image";
 import Link from "next/link";
 import React from "react";
+import { useIsMobile } from "./useIsMobile";
 
 export default function RSComp({ setting }: { setting: Setting }) {
+  const isMobile = useIsMobile();
   return (
-    <div className="fixed bottom-0 right-[50%] translate-x-[-50%] md:bottom-0 md:translate-x-0 md:right-0 px-4 py-4 xl:px-20 md:px-20 mix-blend-difference z-50">
+    <div className="fixed bg-black w-full bottom-0 left-0 flex justify-center p-2 z-50">
       <ul className="flex gap-4 items-center">
         {setting.socials?.map((social) => (
           <li key={social._key}>
@@ -15,7 +18,7 @@ export default function RSComp({ setting }: { setting: Setting }) {
                 <Image
                   src={urlFor(social.social_logo).url()}
                   alt="logo du réseau social"
-                  width={40}
+                  width={isMobile ? 25 : 40}
                   height={40}
                 />
               )}
